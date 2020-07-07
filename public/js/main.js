bcc79547c3554fb4f3dd494147902b4874cefc80
anujsProject <anuj992393@gmail.com>
2020-07-07 11:56:29 +0530

    scroll in animation added

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@
     projectItemArr: Array.from(
       document.querySelectorAll('.projects__list__item')
     ),
+    whyMeContent: document.querySelector('.whyHire__content'),
   };
 
   function setupTypewriter(t) {
@@ -110,4 +111,59 @@
     setTimeout(typewriter.type, 3000);
   });
   ////////////////
+
+  // SCROLL IN IMAGES
+
+  // DEBOUNCE FUNC TO CALL FUNC (AFTER DELAY)
+
+  // FN -> takes function, wait -> for ml before execution
+  const debounce = function (fn, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      const context = this,
+        args = arguments;
+
+      const later = () => {
+        timeout = null;
+        if (!immediate) {
+          fn.apply(context, args);
+        }
+      };
+
+      const callNow = immediate && !timeout;
+
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      timeout = setTimeout(later, wait);
+
+      if (callNow) fn.apply(this, args);
+    };
+  };
+
+  // END OF DEBOUNCE FUNC
+
+  const slideConfig = (item, classActive) => {
+    const itemHeight = Number(getComputedStyle(item).height.split('px')[0]);
+    const itemBottom = item.offsetTop + itemHeight;
+    const slideInAt = window.scrollY + window.innerHeight - itemHeight / 6;
+    const isScrolled = slideInAt > item.offsetTop;
+    const isPastItem = window.scrollY > itemBottom;
+    if (isScrolled && !isPastItem) {
+      item.classList.add(classActive);
+    } else {
+      item.classList.remove(classActive);
+    }
+  };
+
+  const slideInSections = () => {
+    DOMElements.projectItemArr.forEach((item) => {
+      slideConfig(item, 'projects__list__item--active');
+    });
+
+    slideConfig(DOMElements.whyMeContent, 'whyHire__content--active');
+  };
+
+  window.addEventListener('scroll', debounce(slideInSections));
 })();
